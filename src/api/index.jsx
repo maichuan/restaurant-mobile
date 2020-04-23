349b5f70988c6e31a0c77716aff19d04083d8d50
NonPN <non9441@gmail.com>
2020-04-23 15:19:59 +0300

    change end point :bug:

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const serverClient = axios.create({
   // baseURL: 'http://10.2.51.152:3000',
-  baseURL: "http://192.168.43.233:3000",
-  // baseURL: "http://192.168.2.47:3000",
+  // baseURL: "http://192.168.43.233:3000",
+  baseURL: "http://192.168.2.41:3000",
 });
